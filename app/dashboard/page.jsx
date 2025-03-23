a5cab273e87c5d16fe2ac870c70b8564f83114dc
sanyagargg <133576817+sanyagargg@users.noreply.github.com>
2025-03-23 14:31:52 +0530

    justify sign in sign up clerk component

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,8 +1,12 @@
+import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 function dashboard() {
   return (
-    <div>dashboard</div>
+    <div>
+      dashboard
+      <UserButton/>
+    </div>
   )
 }
 
