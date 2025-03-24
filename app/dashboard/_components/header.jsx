cda76d42cfbb131110d00509bafa3225ed8f5720
sanyagargg <133576817+sanyagargg@users.noreply.github.com>
2025-03-24 18:14:30 +0530

    adding header for dashboard

--- app/dashboard/_components/header.jsx
+++ app/dashboard/_components/header.jsx
@@ -1,10 +1,18 @@
+import { UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import React from 'react'
 
 function Header() {
   return (
-    <div>
-        <Image src = {'/logo.svg'} width ={40} height = {40} />
+    <div className='p-5 shadow-sm flex justify-between items-centre'>
+        <div className='flex gap-2 items-center'>
+            <Image src = {'/logo.svg'} width ={40} height = {40} alt="Logo" />
+            <h2 className='font-bold text-lg text-blue-700'>DesignGenie</h2>
+        </div>
+
+        <div>
+            <UserButton/>
+        </div>
     </div>
   )
 }
