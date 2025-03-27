a959c8eedc523c5d1af0368a423117373c7adeb5
sanyagargg <133576817+sanyagargg@users.noreply.github.com>
2025-03-27 16:42:08 +0530

    formatted appearance of image upload component

--- app/dashboard/create-new/_components/ImageUpload.jsx
+++ app/dashboard/create-new/_components/ImageUpload.jsx
@@ -1,11 +1,20 @@
+import Image from 'next/image'
 import React from 'react'
 
 function ImageUpload() {
   return (
     <div>
       <label className='text-blue-800'>Select Image of Your Room</label>
-      <div>
-        <input type = "file" accept="image/*" />
+      <div className='mt-3'>
+        <label htmlFor='upload-image'>
+          <div className={'p-8 w-80 border rounded-xl border-dotted flex justify-center border-purple-400 bg-white cursor-pointer hover:shadow-lg '}>
+            <Image src= "/imageupload.jpeg" width={150} height={150}/>
+          </div>
+        </label>
+        <input type = "file" accept="image/*" 
+        id= "upload-image"
+        style ={{display:"none"}}
+        />
       </div>
     </div>
   )
